345cbd3dd97cc91a5bb3ea0281ea9082e137c464
Rafał Hryniewski <racibor@gmail.com>
2017-02-12 22:52:03 +0100

    Added OWIN Startup

--- Me20/Startup/Startup.cs
+++ Me20/Startup/Startup.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Nancy.Bootstrapper;
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Me20.Web.Startup))]
 
 namespace Me20.Web
 {
     public class Startup
     {
-        
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.UseNancy();
+            //TODO: Create ActorSystem and stuff
+        }
     }
 }
