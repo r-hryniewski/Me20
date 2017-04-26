d646d88265049865a8d0d36ef5ed13728a9e7ff2
Rafał Hryniewski <racibor@gmail.com>
2017-04-26 22:54:14 +0200

    Base64 methods marked as obsolete

--- Me20.Common/Extensions/StringExtensions.cs
+++ Me20.Common/Extensions/StringExtensions.cs
@@ -5,8 +5,9 @@ namespace Me20.Common.Extensions
 {
     public static class StringExtensions
     {
+        [Obsolete("Base64 has slashes so it's useless to ActorPaths, use MD5 instead")]
         public static string ToBase64(this string input) => Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
-
+        [Obsolete("Base64 has slashes so it's useless to ActorPaths, use MD5 instead")]
         public static string FromBase64(this string input) => Encoding.UTF8.GetString(Convert.FromBase64String(input));
     }
 }
