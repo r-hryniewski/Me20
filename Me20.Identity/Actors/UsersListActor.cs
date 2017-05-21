97c320dac9bf1a6d74d498504846f685c73837e3
Rafał Hryniewski <racibor@gmail.com>
2017-05-21 18:28:07 +0200

    UsersListActor added

---
+++ Me20.Identity/Actors/UsersListActor.cs
@@ -0,0 +1,33 @@
+﻿using Akka.Actor;
+using Akka.Persistence;
+using Me20.Common.Abstracts;
+using Me20.Common.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Me20.Identity.Actors
+{
+    public class UsersListActor : ReceivePersistentActorBase
+    {
+        public override string PersistenceId => nameof(UsersListActor).ToMD5();
+
+        private HashSet<string> UserNames {get; set;}
+
+        public UsersListActor() : base()
+        {
+            UserNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            Recover<SnapshotOffer>(offer => UserNames = (HashSet<string>)offer.Snapshot);
+
+            Command<string>(userName => 
+            {
+                if (UserNames.Add(userName))
+                    Persist(userName, un => HandleSnapshoting(UserNames));
+            });
+            Recover<string>(userName => UserNames.Add(userName));
+        }
+
+        public static Props Props => Props.Create(() => new UsersListActor());
+
+    }
+}
--- Me20.Identity/Actors/UsersManagerActor.cs
+++ Me20.Identity/Actors/UsersManagerActor.cs
@@ -5,14 +5,23 @@ using Me20.Common.Interfaces;
 using Me20.Identity.Commands;
 using Me20.Identity.Interfaces;
 using Me20.Identity.QueryMessages;
+using System;
 
 namespace Me20.Identity.Actors
 {
     public class UsersManagerActor : ReceiveActorBase
     {
+        private readonly IActorRef usersListActor;
+
         public UsersManagerActor() : base()
         {
-            Receive<UserLoggedInCommand>(msg => HandleUserLoggedInMessage(msg), 
+            usersListActor = Context.ActorOf(UsersListActor.Props, Guid.NewGuid().ToString());
+
+            Receive<UserLoggedInCommand>(msg => 
+                {
+                    usersListActor.Tell(msg.UserName);
+                    HandleUserLoggedInMessage(msg);
+                }, 
                 msg => msg.IsValid);
 
             Receive<IHaveUserName>(msg => Context.Child(msg.UserName).Forward(msg));
