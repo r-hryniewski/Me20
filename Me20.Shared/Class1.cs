93c028ce0d9c63daea8305451510bd24a76b5996
Rafał Hryniewski <racibor@gmail.com>
2017-08-06 19:54:13 +0200

    Added new projects

---
+++ Me20.Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Me20.Shared
+{
+    public class Class1
+    {
+    }
+}
